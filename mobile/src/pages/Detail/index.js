4e930c7994e1a0af7dc60a59ee76aa544a0a465c
Bert <z0bristbr@gmail.com>
2020-03-26 20:33:47 -0300

    add incident.description

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -52,6 +52,9 @@ export default function Detail() {
         <Text style={styles.incidentProperty}>CASO:</Text>
         <Text style={styles.incidentValue}>{incident.title}</Text>
 
+        <Text style={styles.incidentProperty}>DESCRIÇÃO:</Text>
+        <Text style={styles.incidentValue}>{incident.description}</Text>
+
         <Text style={styles.incidentProperty}>VALOR:</Text>
         <Text style={styles.incidentValue}>
           {Intl.NumberFormat('pt-BR', {
